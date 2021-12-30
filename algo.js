2d2ddf8e4ac247033498fd098f6370df23ad8349
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-30 14:14:17 -0600

    add space before each capital letter in the output

--- algo.js
+++ algo.js
@@ -4,12 +4,11 @@
 
 function solution(str) {
     let output = "";
-    console.log("str", str);
     for ( x in str ) {
-        console.log(str[x]);
         if (str[x] == str[x].toUpperCase()) {
-            console.log("found upper case");
+            output += " ";
         }
+        output += str[x];
         
     }
     return output;
