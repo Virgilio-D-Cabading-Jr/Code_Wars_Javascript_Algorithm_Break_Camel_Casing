2bbbd6e487d1581d75ba4f1e77946d663de43097
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-30 14:08:55 -0600

    iterate through string str

--- algo.js
+++ algo.js
@@ -2,6 +2,14 @@
 //  Code Wars: BREAK CAMEL CASING
 //////////////////////////////////////////////////
 
-function solution(string) {
-    
+function solution(str) {
+    let output = "";
+    console.log("str", str);
+    for ( x in str ) {
+        console.log(str[x]);
+        
+    }
+    return output;
 }
+
+console.log("thisIsAString", solution("thisIsAString"));
